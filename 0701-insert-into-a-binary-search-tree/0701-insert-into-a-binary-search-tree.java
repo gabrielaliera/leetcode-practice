b69a3e9f7655b1b866b8d027bec12e64fbe31d24
gabrielaliera <95945032+gabrielaliera@users.noreply.github.com>
2022-10-27 10:08:05 -0700

    Time: 0 ms (100.00%), Space: 54.1 MB (54.69%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/*1) Base Case: check if the tree is empty. If it is, return a new TreeNode with node value of val
+2) Recursively traverse the tree in pre-order fashion
+    a) Compare the input value with the node values
+        i) If input value > node.val: traverse the right subtree
+        ii) traverse the left subtree
+3) return the root of the tree
+*/
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        
+        //Empty tree
+        if (root == null){
+          return new TreeNode(val);
+        }
+                     
+        
+        if( val < root.val){
+            root.left= insertIntoBST(root.left, val);
+        } else { 
+            root.right =insertIntoBST(root.right,val);
+        }     
+          
+        return root;
+        
+    }
+}

8662da975b9c6b9d9e2608f838fb3543538e54b5
gabrielaliera <95945032+gabrielaliera@users.noreply.github.com>
2023-01-19 22:39:49 -0800

    Time: 2 ms, Memory: 45.1 MB - LeetHub

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -7,28 +7,30 @@ brute force - nested for loops
 
 class Solution {
     public int[] replaceElements(int[] arr) {
-        
-        //base case
-//         if (arr.length == 1 ){
-//             arr[0] = -1;
-            
+                
+//        Brute force
+        //for(int i = 0; i < arr.length ; i++){
+//            int max = Integer.MIN_VALUE;
+           
+//           for( int j = i + 1 ; j < arr.length ; j++){
+//                max = Math.max(max, arr[j]);
+//            }    
+//            arr[i] = max;
 //         }
         
         
-        
-        
-        for(int i = 0; i < arr.length ; i++){
-           int max = Integer.MIN_VALUE;
-           
-          for( int j = i + 1 ; j < arr.length ; j++){
-               max = Math.max(max, arr[j]);
-           }    
-           arr[i] = max;
-        }
-        
-        
-        arr[arr.length - 1] = -1;
+//         arr[arr.length - 1] = -1;
                 
+        
+//         return arr;
+        
+        //Optimized - one pass
+        int max = - 1;
+        for(int i = arr.length - 1 ; i >= 0 ; i--){
+            int num = arr[i];
+            arr[i] = max;
+            max = Math.max(num, max);
+        }
         
         return arr;
     }

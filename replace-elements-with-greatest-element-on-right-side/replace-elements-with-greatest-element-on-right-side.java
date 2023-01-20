5dd49767f4a832a992fe91b06a786247525968b0
gabrielaliera <95945032+gabrielaliera@users.noreply.github.com>
2023-01-19 22:34:37 -0800

    Time: 1057 ms, Memory: 45.3 MB - LeetHub

---
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,35 @@
+/*
+
+brute force - nested for loops
+
+
+*/
+
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        
+        //base case
+//         if (arr.length == 1 ){
+//             arr[0] = -1;
+            
+//         }
+        
+        
+        
+        
+        for(int i = 0; i < arr.length ; i++){
+           int max = Integer.MIN_VALUE;
+           
+          for( int j = i + 1 ; j < arr.length ; j++){
+               max = Math.max(max, arr[j]);
+           }    
+           arr[i] = max;
+        }
+        
+        
+        arr[arr.length - 1] = -1;
+                
+        
+        return arr;
+    }
+}

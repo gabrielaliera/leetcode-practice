9e8024466dd5d23b3e6bfb1552104b1a75bff365
gabrielaliera <95945032+gabrielaliera@users.noreply.github.com>
2023-01-30 12:12:41 -0800

    Time: 2 ms (65.64%), Space: 40.4 MB (67.82%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,33 +1,64 @@
 class Solution {
     public boolean isValid(String s) {
-        final String opening = "({[";
-        final String closing = ")}]";
+//         final String opening = "({[";
+//         final String closing = ")}]";
 
-        Stack<String> myStack = new Stack<>();
+//         Stack<String> myStack = new Stack<>();
     
-        //basecase if string is not even
-        if(s.length() % 2 != 0) return false;
+//         //basecase if string is not even
+//         if(s.length() % 2 != 0) return false;
         
         
-        String[]  array = s.split("");
+//         String[]  array = s.split("");
 
-        for(String c : array){
+//         for(String c : array){
           
-          if(opening.indexOf(c) != -1){ //Symbol found in opening string
-              myStack.push(c);
+//           if(opening.indexOf(c) != -1){ //Symbol found in opening string
+//               myStack.push(c);
           
-          } else if( closing.indexOf(c) != -1){ //Symbol found in closing string
+//           } else if( closing.indexOf(c) != -1){ //Symbol found in closing string
 
-            if(myStack.empty()){
-              return false;
-            }
+//             if(myStack.empty()){
+//               return false;
+//             }
 
-            if(opening.indexOf(myStack.pop()) != closing.indexOf(c)){ //Compare top of stack w/ current c
-              return false;
+//             if(opening.indexOf(myStack.pop()) != closing.indexOf(c)){ //Compare top of stack w/ current c
+//               return false;
+//             }
+//           } //if symbol not found in opening/closing string (ie a,+,b,etc) skip to next element in array
+//         }//end for
+//         return myStack.empty();
+       
+        
+        
+        //Sol #2 with hashmap
+        HashMap<Character, Character> brackets =  new HashMap<>();
+        brackets.put('}','{');
+        brackets.put(']','[');
+        brackets.put(')','(');
+        
+        Stack<Character> stack = new Stack<>();
+        for(Character c : s.toCharArray()){
+            
+            //if has closing bracket
+            if(brackets.containsKey(c)){
+                
+                if(stack.isEmpty()){return false;}
+                
+                //if opening bracket doenst match top of stack
+                if(brackets.get(c) != stack.peek()){
+                    return false;
+                } else{
+                    stack.pop();
+                }
+                
+            } else {
+                //is opening
+                stack.push(c);
             }
-          } //if symbol not found in opening/closing string (ie a,+,b,etc) skip to next element in array
-        }//end for
-        return myStack.empty();
+        }
+        
+        return stack.isEmpty();
         
     }
 }

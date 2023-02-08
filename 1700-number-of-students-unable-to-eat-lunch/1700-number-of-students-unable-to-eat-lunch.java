74e80ca2fae3f9c16534acdcc92045ea4447523f
gabrielaliera <95945032+gabrielaliera@users.noreply.github.com>
2023-02-08 10:48:04 -0800

    Time: 1 ms (75.80%), Space: 40.3 MB (57.90%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,60 @@
+/*
+
+Maybe add all students to queue  / sandwich in stack//can leave in array
+for(student : student)
+  studentQueue.addLast(studnet)
+
+
+
+//student takes sandwhich
+student.removeFirst()
+sandwhicPtr++
+
+//student leave sandwhich move to end of queue
+queue.add(queue.removeFirst())
+
+
+//all student not want the sandwhich
+lastptr == queue.top && sandwhich[i] == quue.top
+  return quque.size()
+
+
+
+return queue.size()
+*/
+
+
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        
+        Queue<Integer> queue = new LinkedList<>();
+        int i = 0; 
+        
+        //add all student to queue
+        for(int stu : students){
+            queue.add(stu);
+        }
+        
+        int returnCount = 0;
+        
+        while(!queue.isEmpty() && i < sandwiches.length && returnCount < queue.size()){
+            
+            //student takes sandwhich
+            if(sandwiches[i] == queue.peek()){
+                queue.remove();
+                i++;
+                returnCount = 0;
+            //student moves to end of queue
+            } else {
+                queue.add(queue.remove());
+                returnCount++;
+            }
+            
+        }
+        
+        
+        return queue.size();
+        
+        
+    }
+}

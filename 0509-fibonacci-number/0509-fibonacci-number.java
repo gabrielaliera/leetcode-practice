afc894e595602f348d0e63d846331cf2c6cd29e2
Gabriela Liera <95945032+gabrielaliera@users.noreply.github.com>
2023-02-27 15:38:40 -0800

    Time: 0 ms (100.00%), Space: 40 MB (18.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,31 @@
 class Solution {
-    public int fib(int n) {
+    public int fib2(int n) {
         if(n <= 1){
             return n;
         }
         
-        return fib(n-1) + fib(n-2);
+        return fib2(n-1) + fib2(n-2);
         
+    }
+    
+    public int fib(int n){
+      
+        if(n <= 1){
+            return n;
+        }
+        
+        
+        int x = 1;
+        int y = 0;
+        int current = 0;
+        
+        for( int i = 2; i <= n ; i++){
+            current = x + y;
+            y =  x;
+            x = current;
+        }
+        
+        
+        return current;
     }
 }

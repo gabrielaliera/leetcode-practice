bec2793943c4a252fe2055abd9a0c19535798fb8
Gabriela Liera <95945032+gabrielaliera@users.noreply.github.com>
2023-04-12 12:42:16 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (24.97%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -19,7 +19,7 @@ class Solution {
          return helper(root);
         
     }
-    
+    //Recursive Depth first search
     public int helper( TreeNode root){
         
         if(root == null){ return 0;}
@@ -28,5 +28,33 @@ class Solution {
        int right = helper(root.right);
            
       return Math.max(left,right) +1 ;
+    }
+    
+    //Iterative DFS
+    public int itDFS (TreeNode root){
+       
+        Queue<TreeNode> queue = new LinkedList<>();
+        Stack<Integer> depths = new Stack<>();
+        queue.add(root);
+        depths.add(1);
+        int depth = 0;
+        int current_depth = 0;
+        
+        while(!queue.isEmpty()){
+            //pop
+            TreeNode cur = queue.remove();
+            current_depth = depths.pop();
+           
+            if(cur != null){
+                depth = Math.max(depth, current_depth);                
+                //add child to queue
+                queue.add(root.left);
+                queue.add(root.right);
+                //add depth
+                depths.add(current_depth +1);
+                depths.add(current_depth +1);
+            }
+        }
+       return depth; 
     }
 }

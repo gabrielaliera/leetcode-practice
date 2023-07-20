b17570698f0f486a2df6ffb8ddc4171d4ff0158e
Gabriela Liera <95945032+gabrielaliera@users.noreply.github.com>
2023-07-20 12:30:08 -0700

    Time: 8 ms (29.26%), Space: 44.1 MB (69.37%) - LeetHub

---
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -0,0 +1,66 @@
+/*
+HashSet
+Plan
+Adj List
+   0 : 1
+   1 : 0 2
+   2 : 1
+   3 : 4
+   4 : 3
+   
+  
+   
+*/
+
+class Solution {
+    
+    
+    
+    public int countComponents(int n, int[][] edges) {
+        
+        //Step 1: Build graph
+        HashMap<Integer, Set<Integer>> adj = new HashMap<>();
+        
+        //Create adj with empty sets
+        for(int i = 0; i < n ; i++){
+            adj.put(i , new HashSet<>());
+        }
+        
+        //Add edges to adj 
+        for(int[] edge : edges){
+            int nodeA = edge[0];
+            int nodeB = edge[1];
+            
+            adj.get(nodeA).add(nodeB);
+            adj.get(nodeB).add(nodeA);           
+        }
+        
+        //Step 2: Calculate number of components usign dfs -
+        //dfs will change all connected nodes to visited 
+        //Once dfs ends, if visited is false again, means a new component
+        boolean [] visited = new boolean[n];
+        int componets = 0;
+        
+        int count=0;
+          for(int i = 0; i < n; i++){
+              if(visited[i] == false) {
+                // apply dfs
+                dfs(adj, visited, i);
+              count++;
+              }
+          }
+         return count;
+    }
+    
+     // use dfs to traverse the graph to see if all the nodes can be covered
+      private void dfs(HashMap<Integer, Set<Integer>> adj, boolean[] visited, int s){
+          //Add node to visited
+          visited[s] = true;
+          
+          for(int v : adj.get(s)){
+              if(visited[v]==false){
+                  dfs(adj,visited,v);
+              }
+          }
+      }
+}

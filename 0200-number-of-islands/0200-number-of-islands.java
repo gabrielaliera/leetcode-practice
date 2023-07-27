fe1c8c1bc7428bdd04b9fa39276343680447909b
Gabriela Liera <95945032+gabrielaliera@users.noreply.github.com>
2023-07-26 19:35:06 -0700

    Time: 3 ms (97.64%), Space: 47.5 MB (79.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -16,37 +16,32 @@
 
 class Solution {
     public int numIslands(char[][] grid) {
+    
         int islands = 0;
-        
-        //locate first island
-        for(int r = 0; r < grid.length; r++){
-            for(int c = 0; c < grid[0].length; c++){
-                                
-                //dfs if find 1
-                if(grid[r][c] == '1'){
+        for(int i = 0; i< grid.length;  i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
                     islands++;
-                    dfs(grid,r,c);
                 }
             }
         }
         return islands;
-        
     }
     
-    
-    public void dfs(char[][]grid, int r, int c){
-        
-        
-        if( r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c]== '0'){
+    public void dfs(char[][] grid, int i , int j){
+        //edges cases
+        if( i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
             return;
         }
-            grid[r][c] = '0';
-            dfs(grid,r-1,c);
-            dfs(grid,r+1,c);
-            dfs(grid,r,c-1);
-            dfs(grid,r,c+1);
         
-     
+        //Sink position
+        grid[i][j] = '0';
         
+        //dfs up, down, left, right
+        dfs(grid, i + 1, j); //down
+        dfs(grid, i - 1, j); //up
+        dfs(grid, i, j + 1); //right
+        dfs(grid, i, j - 1); //left
     }
 }

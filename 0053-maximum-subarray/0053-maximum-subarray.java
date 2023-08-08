242318c64b59fe9c8ce4fc4237739454a4c9bfde
Gabriela Liera <95945032+gabrielaliera@users.noreply.github.com>
2023-08-08 14:00:48 -0700

    Time: 1 ms (100.00%), Space: 59.8 MB (46.75%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,4 +1,22 @@
 class Solution {
+    
+    //DP solution
+    public int maxSubArray(int[] nums) {
+        // initialize our variables using the first element.
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
+        
+        // start with the 2nd element since we already used the first one.
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            // if current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
+        }
+        
+        return maxSubarray;
+    }
+    
     public int maxSubArray2(int[] nums) {
         
         int maxSum = Integer.MIN_VALUE;
@@ -21,7 +39,7 @@ class Solution {
     }
     
     //O(n)
-    public int maxSubArray(int[] nums){
+    public int maxSubArray3(int[] nums){
         if(nums.length == 1){return  nums[0];}
         
         int max = Integer.MIN_VALUE;

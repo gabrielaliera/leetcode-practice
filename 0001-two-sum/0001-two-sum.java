07b4432d028d6003fba2138eb8a3b63ab5f3768b
Gabriela Liera <95945032+gabrielaliera@users.noreply.github.com>
2023-08-23 19:25:34 -0700

    Time: 5 ms (60.84%), Space: 44.1 MB (12.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,21 +1,54 @@
+/*
+
+Understand: Integer overload , 
+
+Brute - iterate all pairs 0(n^2)
+
+with hashmap
+
+a + b = target
+b = target - a
+
+Step 1: create hashmap
+
+Step 2: iterate through array
+
+Step 3: add num to hashmpa
+
+Step 4 check if target is in hashmap
+  of yes, return index
+add values, index to hashmap
+
+Else retunr empty
+
+duplicates:
+
+
+
+*/
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        int result[] = new int[2] ;
+        int[] result = new int[2];
         
-        //value, index
         HashMap<Integer, Integer> map = new HashMap<>();
         
-        for(int i=0;i<nums.length;i++){
+        for(int i = 0 ; i < nums.length ; i ++){
             
-            if(map.containsKey(target-nums[i])){
-                result[0] = i;
-                result[1] = map.get(target-nums[i]);
-            } else {
-                map.put(nums[i],i);
+                      
+            int b = target - nums[i];           
+            
+            if(map.containsKey(b)){
+                result[1] = i;
+                result[0] = map.get(b);
             }
-            
+             
+            map.put(nums[i] , i);
         }
+        
         return result;
+        
     }
 }
